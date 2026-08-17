ec1275b9cd70fb8937d96befe8a05147e6727b40
artem <patriarhgirahov@example.com>
2026-08-17 13:06:37 +0300

    Убрал бюджет, перестроил логику под реальные тарифы

--- states.py
+++ states.py
@@ -2,6 +2,14 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class Diagnostics(StatesGroup):
-    goal = State()
-    problem = State()
-    format = State()
+    # Для тренировок
+    workouts_q1 = State()
+    workouts_q2 = State()
+
+    # Для питания
+    nutrition_q1 = State()
+    nutrition_q2 = State()
+
+    # Для готовой системы
+    system_q1 = State()
+    system_q2 = State()
